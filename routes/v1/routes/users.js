dc89ca23e1325f728091dae6df8e91febd95e13b
Karl Wang <kovl.void@gmail.com>
2017-01-14 17:20:44 -0800

    fixed duplication check

--- routes/v1/routes/users.js
+++ routes/v1/routes/users.js
@@ -37,19 +37,19 @@ router.route('/')
 	
 	//precheck for duplicate
 	//note: sql also checks, but its hard to parse sql error
-	var query = req.pool.query(
-		'SELECT * FROM users WHERE email = ?', req.body.email, function(err, results, fields) {
+	var query = req.pool.query('SELECT * FROM users WHERE email = ?', [req.body.email], function(err, results, fields) {
 		if(err)
 			return next(err);//handle sql error
 		else if(results.length > 0)
 			return next(new Error("user exists with same email"));
-	});
-	
-	//generate salt and hash asyncly
-	bcrypt.hash(req.body.password, 8, function(err, hash) {
-		if(err)
-			return next(err);
-		insert_user(req, res, next, hash);
+		
+		
+		//generate salt and hash asyncly
+		bcrypt.hash(req.body.password, 8, function(err, hash) {
+			if(err)
+				return next(err);
+			insert_user(req, res, next, hash);
+		});
 	});
 })
 .get(function(req, res, next){
