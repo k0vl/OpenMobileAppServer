8e9c9abd137c131af45cc02f83fcf2537b82dd26
Karl Wang <kovl.void@gmail.com>
2017-04-23 00:31:24 -0700

    returns relationship when checking users assocciated with an event

--- routes/v1/routes/events.eventid.users.js
+++ routes/v1/routes/events.eventid.users.js
@@ -7,7 +7,12 @@ var modify = require('./util/modify');
 router.route('/')
 .get(function(req, res, next){
 	//'SELECT * FROM user_event WHERE event_id = ?'
-	modify.custom_query_all(req, res, next, 'SELECT * FROM users WHERE id IN (SELECT user_id FROM user_event WHERE event_id = ? )', [req.event.id]);
+	//'SELECT * FROM users WHERE id IN (SELECT user_id FROM user_event WHERE event_id = ? )'
+	var queryString = "SELECT users.*, user_event.relationship \
+	FROM users JOIN user_event \
+	ON users.id = user_event.user_id \
+	WHERE user_event.event_id = ?"
+	modify.custom_query_all(req, res, next, queryString, [req.event.id]);
 });
 
 module.exports = router;
