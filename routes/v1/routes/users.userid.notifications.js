221ee615d3e3ce7e7639f4fe662b764bf51a4847
Karl Wang <kovl.void@gmail.com>
2017-05-21 15:18:32 -0700

    get notification by user id

--- routes/v1/routes/users.userid.notifications.js
+++ routes/v1/routes/users.userid.notifications.js
@@ -8,6 +8,10 @@ router.route('/')
 .post(function(req, res, next) {
 	//use custom_query_by_array to prevent null insertion problems
 	modify.custom_query_by_array(req, res, next, 'INSERT INTO notification SET user_id=' + req.pool.escape(req.user.id) + ', ?', ["message","type"]);
-});
+})
+.get(function(req, res, next){
+	//'SELECT user1_id FROM following WHERE user2_id = ?'  , "user1_id"
+	modify.custom_query_all(req, res, next,  'SELECT * FROM notification WHERE user_id = ?', [req.user.id]);
+})
 
 module.exports = router;
