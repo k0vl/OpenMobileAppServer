64672e853e047e91d4c35b342079cf2cea61d5b2
Karl Wang <kovl.void@gmail.com>
2017-05-27 15:38:48 -0700

    compare timestamp

--- routes/v1/routes/events.find-by-coordinate.js
+++ routes/v1/routes/events.find-by-coordinate.js
@@ -6,8 +6,20 @@ var modify = require('./util/modify');
 /* GET users listing. */
 router.route('/')
 .get(function(req, res, next){
-	modify.custom_query_all(req, res, next, 'SELECT * FROM events WHERE lat > ? AND lat < ? AND lng > ? AND lng < ?',
-	[req.query.min_lat, req.query.max_lat, req.query.min_lng, req.query.max_lng]);
+	var queryString = 'SELECT * FROM events WHERE lat > ? AND lat < ? AND lng > ? AND lng < ?';
+	var queryArray = [req.query.min_lat, req.query.max_lat, req.query.min_lng, req.query.max_lng];
+
+	if(req.query.start_after){
+		queryString += ' AND start_timestamp > ?';
+		queryArray.push(req.query.start_after);
+	}
+
+	if(req.query.end_after){
+		queryString += ' AND end_timestamp > ?';
+		queryArray.push(req.query.end_after);
+	}
+
+	modify.custom_query_all(req, res, next, queryString, queryArray);
 });
 
 module.exports = router;
