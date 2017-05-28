1528340f26a214e7c57adf6e791fb8b84074d1b8
Karl Wang <kovl.void@gmail.com>
2017-05-27 17:35:14 -0700

    update relationship

--- routes/v1/routes/events.eventid.users.userid.js
+++ routes/v1/routes/events.eventid.users.userid.js
@@ -5,7 +5,7 @@ var modify = require('./util/modify');
 
 router.route('/')
 .post(function(req, res, next) {
-	modify.custom_query_all(req, res, next, 'INSERT INTO user_event SET ?', 
+	modify.custom_query_all(req, res, next, 'INSERT INTO user_event SET ? ON DUPLICATE KEY UPDATE relationship=VALUES(relationship)', 
 		{user_id:req.user.id, event_id:req.event.id, relationship:req.body.relationship});
 })
 .delete(function(req, res, next) {
