5bcc98485fcd4dcc5512f0cdadf3c013fb1f4d9f
Karl Wang <kovl.void@gmail.com>
2017-06-09 01:37:28 -0700

    optional email and password

--- routes/v1/routes/users.js
+++ routes/v1/routes/users.js
@@ -8,30 +8,51 @@ var modify = require('./util/modify');
 router.route('/')
 .post(function(req, res, next){
 	
-	if(!req.body.email)
-		return next(new Error("no email address"));
-	else if(!req.body.password)
-		return next(new Error("no password"));
+	var fb_id = req.body.fb_id;
+	var email = req.body.email;
+	var password = req.body.password;
+	if(!fb_id){
+		if(!email && !password)
+			return next(new Error("no identification (provide either fb_id, or email and password"));
+		else if(!email && password)
+			return next(new Error("no email"));
+		else if(email && !password)
+			return next(new Error("no password"));
+	}
 	
 	//precheck for duplicate
 	//note: sql also checks, but its hard to parse sql error
-	var query = req.pool.query('SELECT * FROM users WHERE email = ?', [req.body.email], function(err, results, fields) {
+	var qs = 'SELECT * FROM users WHERE ';
+	var qo = [];
+	if(fb_id){
+		qs += 'fb_id = ?';
+		qo.push(fb_id);
+	}
+	if(fb_id && email){
+		qs += ' OR ';
+	}
+	if(email){
+		qs += 'email = ?';
+		qo.push(email);
+	}
+	var query = req.pool.query(qs, qo, function(err, results, fields) {
 		if(err)
 			return next(err);//handle sql error
 		else if(results.length > 0)
-			return next(new Error("user exists with same email"));
+			return next(new Error("user exists with same identification"));
 		
-		//generate salt and hash asyncly
-		bcrypt.hash(req.body.password, 8, function(err, hash) {
-			if(err)
-				return next(err);
-			modify.modify_user(req, res, next, 'INSERT INTO users SET ?, hash=' + req.pool.escape(hash));
-		});
+		if(password){
+			//generate salt and hash asyncly
+			bcrypt.hash(req.body.password, 8, function(err, hash) {
+				if(err) return next(err);
+				modify.modify_user(req, res, next, 'INSERT INTO users SET ?, hash=' + req.pool.escape(hash));
+			});
+		} else if (fb_id) {
+			modify.modify_user(req, res, next, 'INSERT INTO users SET ?');
+		} else {
+			return next(new Error("how did you get here??"));
+		}
 	});
-})
-.get(function(req, res, next){
-	res.locals.data.message = "testing only; this should not be accessed during production";
-	return res.json(res.locals);
 });
 
 module.exports = router;
