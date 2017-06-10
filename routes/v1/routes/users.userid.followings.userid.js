fe9785c2fd96161c5c89db8efb6c3b27e2e03b0c
Karl Wang <kovl.void@gmail.com>
2017-06-09 19:15:53 -0700

    comma

--- routes/v1/routes/users.userid.followings.userid.js
+++ routes/v1/routes/users.userid.followings.userid.js
@@ -5,10 +5,10 @@ var modify = require('./util/modify');
 
 router.route('/')
 .post(function(req, res, next) {
-	modify.custom_query_all(req, res, next, 'INSERT INTO following SET ?',{user1_id:req.user.id, user2_id:req.user2.id});
+	modify.custom_query_all(req, res, next, 'INSERT INTO following SET ?', {user1_id:req.user.id, user2_id:req.user2.id});
 })
 .delete(function(req, res, next) {
-	modify.custom_query_all(req, res, next, 'DELETE FROM following WHERE user1_id = ? AND user2_id = ?' [req.user.id, req.user2.id]);
+	modify.custom_query_all(req, res, next, 'DELETE FROM following WHERE user1_id = ? AND user2_id = ?', [req.user.id, req.user2.id]);
 })
 
 module.exports = router;
