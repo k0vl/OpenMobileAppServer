39857037bb1aa62759b0dd15077cd08426d0b895
Karl Wang <kovl.void@gmail.com>
2017-06-27 01:08:37 -0700

    return info for notification

--- routes/v1/routes/users.userid.notifications.notificationid.js
+++ routes/v1/routes/users.userid.notifications.notificationid.js
@@ -7,7 +7,7 @@ var modify = require('./util/modify');
 router.route('/')
 .get(function(req, res, next){
 	//'SELECT user1_id FROM following WHERE user2_id = ?'  , "user1_id"
-	modify.custom_query_all(req, res, next,  'SELECT * FROM notification WHERE id = ?', [req.notification.id]);
+	modify.custom_query_1_row_with_info(req, res, next,  'SELECT * FROM notification WHERE id = ?', [req.notification.id]);
 })
 .delete(function(req, res, next) {
 	modify.custom_query_all(req, res, next, 'DELETE FROM notification WHERE id = ?', [req.notification.id]);
--- routes/v1/routes/util/modify.js
+++ routes/v1/routes/util/modify.js
@@ -1,5 +1,7 @@
 //'INSERT INTO users SET ?'
 
+async = require("async");
+
 module.exports.custom_query_all = function (req, res, next, queryString, queryObject){
 	//TODO:generalize the post params
 	//TODO:automate the query creation with reflection or something
@@ -27,6 +29,54 @@ module.exports.custom_query_1_col = function(req, res, next, queryString, queryO
 		if(err) {return next(err);}
 		res.locals.data = results.map( function(item) {return item[columnName];} );
 		res.json(res.locals);
+	});
+	console.log(query.sql);
+}
+
+module.exports.custom_query_1_row_with_info = function (req, res, next, queryString, queryObject) {
+	var query = req.pool.query(queryString, queryObject, function(err, results, fields) {
+		if(err) {return next(err);}
+		else if(results.length != 1)
+			return next(new Error("not found"));
+		res.locals.data = results[0];
+
+		var p1 = function(callback) {
+			if(results[0].sender_id !== null){
+				req.pool.query('SELECT * FROM users WHERE id = ?', [results[0].sender_id], function(err, results, fields) {
+					if(err !== null){callback(err);}
+					else if (results.length != 1){callback(new Error("sender not found"));}
+					else{callback(null, results[0]);}
+				})
+			} else {
+				callback(null, null);
+			}
+		}
+
+		var p2 = function(callback) {
+			if(results[0].event_id !== null){
+				req.pool.query('SELECT * FROM events WHERE id = ?', [results[0].event_id], function(err, results, fields) {
+					if(err !== null){callback(err);}
+					else if (results.length != 1){callback(new Error("event not found"));}
+					else{callback(null, results[0]);}
+				})
+			} else {
+				callback(null, null);
+			}
+		}
+
+		async.parallel([p1,p2], function(err, results) {
+			if(err){
+				next(err);
+				return;
+			}
+			if(results[0] !== null){
+				res.locals.data.sender = results[0];
+			}
+			if(results[1] !== null){
+				res.locals.data.event = results[1];
+			}
+			res.json(res.locals);
+		});
 	});
 	console.log(query.sql);
 }
